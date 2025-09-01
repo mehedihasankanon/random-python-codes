ab51feec598a28395a92d4205e682d886bb5e37c
Mehedi Hasan Kanon <mehedi.hkanon@gmail.com>
2025-09-02 02:39:12 +0600

    problem set 2 done. kinda

--- MIT_6_0002/PS1/ps1b.py
+++ MIT_6_0002/PS1/ps1b.py
@@ -10,6 +10,23 @@
 #================================
 
 # Problem 1
+
+def process_dp(egg_weights = [], current_index = 0, current_weight = 0, current_number = 0, target_weight = 0, memo = {}):
+    
+    if current_index >= len(egg_weights):
+        return
+    
+    if current_weight > target_weight:
+        return
+    
+    memo[current_weight] = min(memo.get(current_weight, 99999999999), current_number)
+    
+    process_dp(egg_weights, current_index, current_weight + egg_weights[current_index], current_number + 1, target_weight, memo)
+    process_dp(egg_weights, current_index + 1, current_weight, current_number, target_weight, memo)
+    
+        
+
+
 def dp_make_weight(egg_weights, target_weight, memo = {}):
     """
     Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
@@ -22,8 +39,11 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     
     Returns: int, smallest number of eggs needed to make target weight
     """
-    # TODO: Your code here
-    pass
+    # this is basically the coin change problem LMAO
+    
+    process_dp(list(egg_weights), 0, 0, 0, target_weight, memo)
+    print(memo)
+    return memo.get(target_weight, -1)
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
